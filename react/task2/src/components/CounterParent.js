e8ca9ff6e145786fe8efa4e401d11cac119cbae9
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-07 17:50:59 +0300

    Added lifecycle methods

--- react/task2/src/components/CounterParent.js
+++ react/task2/src/components/CounterParent.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import CounterContainer from "./CounterContainer";
 import { CounterContainerStyle, TextStyle, GridStyle } from '../views/Counter/style'
+import scrollToComponent from 'react-scroll-to-component';
 
 
 const initialState = [{ count: 0, id: 1 }]
@@ -11,18 +12,48 @@ class CounterParent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            counters: [{ count: 0, id: 1 }]
+            counters: initialState
         };
 
         this.handleIncrement = this.handleIncrement.bind(this);
         this.handleDecrement = this.handleDecrement.bind(this);
         this.handleReset = this.handleReset.bind(this);
+
+        console.log("constructor:CounterParent");
     }
 
-    componentDidMount() { //////////////readread
-        this.setState({
-            counters: initialState,
-        })
+    componentDidMount() {
+        console.log("componentDidMount:CounterParent");
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate:CounterParent");
+        return (nextState.counters !== this.state.counters);
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("getSnapshotBeforeUpdate:CounterParent")
+        if (prevState.counters.length > 30 && prevState.counters.length < this.state.counters.length) {
+            const element = document.querySelector("#counters:last-child");
+            return element;
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("componentDidUpdate:CounterParent")
+        if (snapshot !== null) {
+            console.log(snapshot);
+            scrollToComponent(snapshot, {
+                offset: 1000,
+                align: 'bottom',
+                duration: 1500
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount:CounterParent");
     }
 
     randomInteger = (min, max) => {
@@ -82,14 +113,15 @@ class CounterParent extends React.Component {
 
     handleRemoveCounter = () => {
         let newCounters = this.state.counters;
-        if (newCounters.length > 1)
+        if (newCounters.length > 1) {
             newCounters = newCounters.map((counter) => {
                 if (counter.count % 2 !== 0 && counter.count !== 0) {
                     return ({ count: counter.count - 1, id: counter.id })
                 }
                 return counter;
             })
-        newCounters.pop();
+            newCounters.pop();
+        }
         this.setState({
             counters: newCounters
         });
@@ -102,15 +134,7 @@ class CounterParent extends React.Component {
     }
 
     render() {
-        // let counters = this.state.counters.map((counterObj) => 
-        //     <CounterContainer
-        //         handleReset={this.handleReset}
-        //         handleIncrement={this.handleIncrement}
-        //         handleDecrement={this.handleDecrement}
-        //         key={counterObj.id}
-        //         counterState={counterObj}
-        //     />
-        // )
+        console.log("render:CounterParent")
         return (
             <React.Fragment>
                 <p style={TextStyle}>{this.state.counters.length}</p>
@@ -125,7 +149,7 @@ class CounterParent extends React.Component {
                         <Button onClick={() => this.handleResetCounter()}>reset counter</Button>
                     </Grid>
                 </Grid>
-                <div style={CounterContainerStyle}>
+                <div id="counters" style={CounterContainerStyle}>
                     {
                         this.state.counters.map((counterObj) =>
                             <CounterContainer
