56098e92a296159dd1ed196ac3e3a2411aae33d2
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-10 15:25:36 +0300

    Modified structure, added 404 page

--- react/task3/src/views/App.js
+++ react/task3/src/views/App.js
@@ -3,15 +3,14 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import CounterParent from '../components/CounterParent';
 import About from "./About";
 import Page404 from "./Page404";
-import Content from "./Content";
+import Header from './Header';
+import Main from './Main';
 
 const App = () => (
-    <Switch>
-        <Route path='/' component={Content} />
-        <Route path='/about' component={About} />
-        <Route path='/counters' component={CounterParent} />
-        <Route path="" component={Page404} />
-    </Switch>
+    <div>
+        <Header />
+        <Main />
+    </div>
 )
 
 export default App;
--- react/task3/src/views/Main.js
+++ react/task3/src/views/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import CounterParent from '../components/CounterParent';
 import About from "./About";
+import App from "./App";
 import Page404 from "./Page404";
 
 const Main = () => (
@@ -9,6 +10,7 @@ const Main = () => (
         <Switch>
             <Route path='/about' component={About} />
             <Route path='/counters' component={CounterParent} />
+            <Route path='/*' component={Page404} />
         </Switch>
     </main>
 )
