e4a2bb8b18f9d7a412b63613f56a0cea9914e6b5
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-17 17:21:58 +0300

    Updated cParent ids

--- react/task4/src/components/Counter/CounterParent.js
+++ react/task4/src/components/Counter/CounterParent.js
@@ -5,7 +5,7 @@ import CounterContainer from "./CounterContainer";
 import { CounterContainerStyle, TextStyle, GridStyle } from '../../style'
 import scrollToComponent from 'react-scroll-to-component';
 
-const initialState = [{ count: 0, id: 1 }]
+const initialState = [{ count: 0, id: 0 }]
 
 class CounterParent extends React.Component {
     constructor(props) {
