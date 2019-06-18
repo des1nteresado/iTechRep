fe145fa85271faf7930a24aeb8eba06199685adb
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-18 23:58:10 +0300

    initial commit

---
+++ WebApiTask1/WebApiTask1/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApiTask1.Controllers
+{
+    [Route("sum")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("{a}/{b}.{format}"), FormatFilter]
+        public ActionResult Get(int a, int b)
+        {
+            var values = new Dictionary<string, int>(3) { { "a", a }, { "b", b }, { "sum", a + b } };
+            var myJsonValues = JsonConvert.SerializeObject(values);
+            return Content(myJsonValues, "application/json");
+        }
+    }
+}
