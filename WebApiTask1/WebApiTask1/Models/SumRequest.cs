c52cd5a31bafad673b13c42e71c63a93c8b7b48d
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-21 14:24:39 +0300

    Restored WebApiTask1

---
+++ WebApiTask1/WebApiTask1/Models/SumRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTask1.Models
+{
+    public class SumRequest
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Param. A must be a positive.")]
+        public int? A { get; set; }
+
+        [Range(int.MinValue, -1, ErrorMessage = "Param. B must be a negative.")]
+        public int? B { get; set; }
+
+        public int? Sum => A + B;
+    }
+}
